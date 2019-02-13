7626c6c9ac71d3c4aebbcd4844f8c123636c8824
prae-torius <arnettepreston@gmail.com>
2019-02-13 12:35:14 -0500

    improve responsive design, user experience. add fetch-request error handling.

--- main.js
+++ main.js
@@ -14,19 +14,15 @@ function watchForm() {
     const queryArtist = $('#query-artist').val();
 
     $('#search-results').remove();
-    $('#player-container').remove();
+    $('#player-section').remove();
     $('#lyrics-section').remove();
 
     // responsible for fetching and displaying search results and lyrics
     getVideosFromQuery(querySong, queryArtist);
     getLyrics(querySong, queryArtist);
     
-    // hides the 'How to guide' and clears video
+    // hides the 'How to guide'
     $('#how-to').addClass('hidden');
-    $('#how-to-toggle').remove();
-    
-    // responsible for adding text to the header that allows the 'How to guide' to be toggled
-    // displayHowTo();
   })
 }
 
@@ -38,7 +34,7 @@ function formatQueryParams(params) {
 function getVideosFromQuery(querySong, queryArtist){
   const params = {
     key: apiKey,
-    maxResults: '50',
+    maxResults: '15',
     part: 'snippet',
     q: `${querySong} ${queryArtist}`,
     type: 'video',
@@ -58,7 +54,7 @@ function getVideosFromQuery(querySong, queryArtist){
   })
   .then(responseJson => displaySearchResults(responseJson))
   .catch(err => {
-    alert(`Unable to get search results: ${err.message}`)
+    alert(`Unable to get search results: ${err.message}`);
   })
 }
 
@@ -68,9 +64,9 @@ function getVideosFromQuery(querySong, queryArtist){
 
 function displaySearchResults(responseJson) {
 // display search results
-  $('#how-to').after('<section id="search-results" class="results"><h3>Video List</h3><p id="#search-error"></p><ul id="results-list"></ul></section>')
+  $('#how-to').after('<section id="search-results" class="results"><h3 class="result-title red-font">Video List</h3><p id="#search-error"></p><ul id="results-list"></ul></section>')
   for(let i = 0; i < responseJson.items.length; i++){
-    $('#results-list').append(`<li id="${responseJson.items[i].id.videoId}"><p>${responseJson.items[i].snippet.title}</p></li>`);
+    $('#results-list').append(`<li id="${responseJson.items[i].id.videoId}"><a href="#player-section" class="list-item">${responseJson.items[i].snippet.title}</a></li>`);
   }
 }
 
@@ -80,8 +76,8 @@ function displaySelectedResult() {
   $('main').on('click', 'li', event =>{
     event.preventDefault();
     console.log('clicked');
-    $('#player-container').remove();
-    $('#search-results').after(`<section id="player-container" class="results"><h3>Video</h3><iframe width="480" height="270" src="https://www.youtube.com/embed/${$(event.currentTarget).attr('id')}" frameborder="0" encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><p>Unfortunately, some songs on Youtube are not be allowed to be played in an embedded player and will be unavailable</p><a href="https://www.youtube.com/watch?v=${$(event.currentTarget).attr('id')}" target="_blank">Open video in new tab</a></section>`);
+    $('#player-section').remove();
+    $('#search-results').after(`<section id="player-section" class="results"><h3 class="result-title red-font">Video</h3><iframe width="480" height="270" src="https://www.youtube.com/embed/${$(event.currentTarget).attr('id')}" frameborder="0" encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><p>Unfortunately, some songs on Youtube are not be allowed to be played in an embedded player and will be unavailable :(</p><a href="https://www.youtube.com/watch?v=${$(event.currentTarget).attr('id')}" target="_blank">Open video in new tab</a></section>`);
   })
 }
 
@@ -99,43 +95,49 @@ function getLyrics(querySong, queryArtist) {
   .then(responseJson => displayLyrics(responseJson))
   .catch(err => {
     alert(`Unable to get lyrics: ${err.message}`);
-    displayLyricsError(err)
+    displayLyricsError(err);
   })
 }
 
 function displayLyrics(responseJson) {
   const lyricString = responseJson.lyrics.replace(/\n/ig, '<br>');
   
-  $('main').append(`<section id="lyrics-section" class="results"><h3>Lyrics</h3><div id="lyrics-container" class="center"><p id="lyrics-error"></p></div></section>`)
+  $('main').append(`<section id="lyrics-section" class="results"><h3 class="result-title red-font">Lyrics</h3><div id="lyrics-container" class="center"><p id="lyrics-error"></p></div></section>`)
   $('#lyrics-container').html(lyricString);
 }
 
 function displayLyricsError(err) {
   // genereates HTML for showing the user that an error has occured with getting lyrics
-  $('main').append(`<section id="lyrics-error" class="results"><p id="lyrics-error">${`Unable to get lyrics: ${err.message}`}</p></section`)
+  $('main').append(`<section id="lyrics-section" class="results"><h3 class="result-title red-font">Lyrics</h3><p id="lyrics-error">${`<span class="red-font">Unable to get lyrics:</span> ${err.message}<br><br>You most likely didn't enter the correct <span class="red-font">SONG TITLE</span> or <span class="red-font">ARTIST NAME</span>`}</p></section>`)
 }
 
-function displayHowTo() {
-  // Displays step-by-step how to use the app
-  $('header').append('<h2 id="how-to-toggle">How to</h2>');
-}
+// function displayHowTo() {
+//   // Displays step-by-step how to use the app
+//   $('header').append('<h2 id="how-to-toggle">How to</h2>');
+// }
 
-function toggleHowTo() {
-  $('header').on('click', '#how-to-toggle', event => {
-    $('#how-to').toggleClass('hidden');
-    toggleResultsVisibility();
+// function toggleHowTo() {
+//   $('header').on('click', '#how-to-toggle', event => {
+//     $('#how-to').toggleClass('hidden');
+//     toggleResultsVisibility();
+//   })
+// }
+
+// function toggleResultsVisibility() {
+//   $('#search-results').toggleClass('results');
+//   $('#player-section').toggleClass('results');
+//   $('#lyrics-section').toggleClass('results');
+//   $('#results-list').toggleClass('hidden');
+//   $('#player-section').toggleClass('hidden');
+//   $('#lyrics-section').toggleClass('hidden');
+// }
+
+function scrollToID() {
+  $('body').on('click', 'li', event => {
+    $("html, body").animate({ scrollTop: $("#player-section").offset().top }, 500);
   })
 }
 
-function toggleResultsVisibility() {
-  $('#search-results').toggleClass('results');
-  $('#player-container').toggleClass('results');
-  $('#lyrics-section').toggleClass('results');
-  $('#results-list').toggleClass('hidden');
-  $('#player-container').toggleClass('hidden');
-  $('#lyrics-section').toggleClass('hidden');
-}
-
+$(scrollToID);
 $(watchForm);
 $(displaySelectedResult);
-$(toggleHowTo);
